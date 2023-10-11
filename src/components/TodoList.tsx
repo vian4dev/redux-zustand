373eb1edd98ea8dc95620312280adf5557d12f8b
Gabriel Viana <viannadoze@gmail.com>
2023-10-11 16:06:31 -0300

    Disparando actions no Redux

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,9 @@
-import { useSelector } from "react-redux";
+import { useAppSelector } from "../store";
 
 
 
 export function TodoList() {
-    const todos = useSelector(store => {
+    const todos = useAppSelector(store => {
         return store.todo
     })
 
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,14 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { useSelector, TypedUseSelectorHook } from "react-redux";
 
 const todoSlice = createSlice({
     name: 'todo',
-    initialState: ['Fazer Café', 'Estudar Reducer'],
-    reducers: {},
+    initialState: ['Fazer Café', 'Estudar Reducer', 'Estudar Zustand'],
+    reducers: {
+        add: (state, action) => {
+            state.push(action.payload.newTodo)
+        },
+    },
 })
 
 export const store = configureStore({
@@ -11,3 +16,8 @@ export const store = configureStore({
         todo: todoSlice.reducer,
     }
 })
+
+export const { add } = todoSlice.actions
+
+export type RootState = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
