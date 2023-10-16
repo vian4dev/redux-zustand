080f9246db738b854307fdcf00fc2dc4f0b692a0
Gabriel Viana <viannadoze@gmail.com>
2023-10-15 21:59:47 -0300

    Radix Collapsible nos módulos

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,28 +1,38 @@
-import { ChevronDown, Video } from "lucide-react";
+import * as Collapsible from "@radix-ui/react-collapsible";
+
+import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 
+interface ModuleProps {
+    moduleIndex: number
+    title: string
+    amountOfLessons: number
+}
 
-export function Module() {
+
+export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
     return (
-        <div>
+        <Collapsible.Root className="group">
 
-            <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+            <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
                 <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-                    1
+                    {moduleIndex + 1}
                 </div>
                 <div className="flex flex-col gap-1 text-left">
-                    <strong className="text-sm">Desvendando o Redux</strong>
-                    <span className="text-xs text-zinc-400">12 aulas</span>
+                    <strong className="text-sm">{title}</strong>
+                    <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
                 </div>
 
-                <ChevronDown className="w-4 h-4 ml-auto text-zinc-400" />
-            </button>
+                <ChevronDown className="w-4 h-4 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+            </Collapsible.Trigger>
 
-            <nav className="relative flex flex-col gap-4 p-6">
-                <Lesson />
-                <Lesson />
-                <Lesson />
-            </nav>
-        </div>
+            <Collapsible.Content>
+                <nav className="relative flex flex-col gap-4 p-6">
+                    <Lesson title="Fundamentos do Redux" duration="09:13" />
+                    <Lesson title="Fundamentos do Redux" duration="09:13" />
+                    <Lesson title="Fundamentos do Redux" duration="09:13" />
+                </nav>
+            </Collapsible.Content>
+        </Collapsible.Root>
     )
 }
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, MessageCircle } from 'lucide-react'
+import { MessageCircle } from 'lucide-react'
 import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
@@ -21,9 +21,9 @@ export function Player() {
                         <Video />
                     </div>
                     <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-                        <Module />
-                        <Module />
-                        <Module />
+                        <Module moduleIndex={0} title="Desvendando o Redux" amountOfLessons={3}/>
+                        <Module moduleIndex={1} title="Desvendando o Redux" amountOfLessons={3}/>
+                        <Module moduleIndex={2} title="Desvendando o Redux" amountOfLessons={3}/>
                     </aside>
                 </main>
             </div>
