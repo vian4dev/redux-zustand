87e38bdcfd66cf8781466bae903fc3661599a864
Gabriel Viana <viannadoze@gmail.com>
2023-10-17 15:16:53 -0300

    Selecionando aula atual

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -3,6 +3,8 @@ import * as Collapsible from "@radix-ui/react-collapsible";
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface ModuleProps {
     moduleIndex: number
@@ -12,6 +14,8 @@ interface ModuleProps {
 
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+    const dispatch = useDispatch() 
+    
     const lessons = useAppSelector((state) => {
         return state.player.course.modules[moduleIndex].lessons
     })
@@ -33,12 +37,13 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
             <Collapsible.Content>
                 <nav className="relative flex flex-col gap-4 p-6">
-                    {lessons.map(lesson => {
+                    {lessons.map((lesson, lessonIndex) => {
                         return (
                             <Lesson 
                             key={lesson.id} 
                             title={lesson.title} 
                             duration={lesson.duration}
+                            onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
                             />
                         )
                     })}
