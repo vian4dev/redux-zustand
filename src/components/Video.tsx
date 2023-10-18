b946570e9b99a5d01441e3164fc8965bc2c2473a
Gabriel Viana <viannadoze@gmail.com>
2023-10-18 11:15:46 -0300

    Criando hook global

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,16 +1,17 @@
 import ReactPlayer from "react-player";
-import { useAppSelector } from "../store";
+
+import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 
 export function Video() {
-    const lesson = useAppSelector(state => {
-        const { currentModuleIndex, currentLessonIndex } = state.player
+    const dispatch = useDispatch()
 
-        const currentLesson = 
-        state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+    const { currentLesson } = useCurrentLesson()
 
-        return currentLesson
-    })
+    function handleNextPlay() {
+        dispatch(next())
+    }
     
     return (
         <div className="w-full bg-zinc-950 aspect-video">
@@ -18,7 +19,9 @@ export function Video() {
                 width="100%"
                 height="100%"
                 controls
-                url={`https://www.youtube.com/watch?v=${lesson.id}`}
+                playing
+                onEnded={handleNextPlay}
+                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
             />
         </div>
     )
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,12 +3,19 @@ import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
 import { useAppSelector } from '../store'
+import { useEffect } from 'react'
+import { useCurrentLesson } from '../store/slices/player'
 
 export function Player() {
     const modules = useAppSelector(state => {
         return state.player.course.modules
     })
 
+    const { currentLesson } =useCurrentLesson()
+
+    useEffect(() => {
+        document.title = `Àssistindo: ${currentLesson.title}`
+    }, [currentLesson])
     return (
         <div className=" h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
             <div className="flex w-[1000px] flex-col gap-6">
