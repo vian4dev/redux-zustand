bde3330b85af58b5e7a1ab7c939367ab76784f0d
Gabriel Viana <viannadoze@gmail.com>
2023-10-23 16:39:14 -0300

    Carregando dados do curso

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -12,6 +12,10 @@ export function Video() {
     function handleNextPlay() {
         dispatch(next())
     }
+
+    if (!currentLesson) {
+        return null
+    }
     
     return (
         <div className="w-full bg-zinc-950 aspect-video">
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -4,17 +4,29 @@ import { Video } from '../components/Video'
 import { Module } from '../components/Module'
 import { useAppSelector } from '../store'
 import { useEffect } from 'react'
-import { useCurrentLesson } from '../store/slices/player'
+import { start, useCurrentLesson } from '../store/slices/player'
+import { api } from '../lib/axios'
+import { useDispatch } from 'react-redux'
 
 export function Player() {
+    const dispatch = useDispatch()
+
     const modules = useAppSelector(state => {
-        return state.player.course.modules
+        return state.player.course?.modules
     })
 
     const { currentLesson } =useCurrentLesson()
 
     useEffect(() => {
-        document.title = `Àssistindo: ${currentLesson.title}`
+        api.get('/courses/1').then(response => {
+           dispatch(start(response.data))
+        })
+    }, [])
+
+    useEffect(() => {
+        if (currentLesson) {
+            document.title = `Àssistindo: ${currentLesson.title}`
+        }
     }, [currentLesson])
     return (
         <div className=" h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
@@ -33,14 +45,13 @@ export function Player() {
                         <Video />
                     </div>
                     <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-                        {modules.map((module, index) => {
+                        {modules && modules.map((module, index) => {
                             return (
                                 <Module 
                                 key={module.id}
                                 moduleIndex={index}
                                 title={module.title}
                                 amountOfLessons={module.lessons.length}
-                                
                                 />
                             )
                         })}
